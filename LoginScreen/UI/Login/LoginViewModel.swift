585986e7f5a5f06ade6cd05570ecd0ceed1cc357
mspoonam <mspoonam.learning@gmail.com>
2022-10-26 22:15:03 -0400

    loginResponse from `login-api-response` json

--- LoginScreen/UI/Login/LoginViewModel.swift
+++ LoginScreen/UI/Login/LoginViewModel.swift
@@ -7,15 +7,56 @@
 
 import Foundation
 
-public class LoginViewModel: ObservableObject {
-    @Published var loginForm = LoginFormState(username: "", password: "")
-    
-    public func authenticateUser() {
-        
-    }
-}
-
 struct LoginFormState {
     var username: String = ""
     var password: String = ""
+}
+
+struct LoginResponse: Codable {
+    let Token: String?
+    let message: String?
+    let display_msg: String?
+}
+
+public class LoginViewModel: ObservableObject {
+    @Published var loginForm = LoginFormState(username: "", password: "")
+    @Published var navigate = false
+    
+    public func authenticateUser() {
+        let loginReponse = getLoginResponse()
+        if loginReponse?.Token?.isEmpty == false {
+            self.navigate = true
+        }
+    }
+    
+    func getLoginResponse() -> LoginResponse? {
+        if let file = readLocalFile(forName: "login-api-response"), let response = parse(jsonData: file) {
+            return response
+        }
+        return nil
+    }
+    
+    func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
+               let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+   
+    func parse(jsonData: Data) -> LoginResponse? {
+        do {
+            let decodedData = try JSONDecoder().decode(LoginResponse.self, from: jsonData)
+            return decodedData
+        } catch {
+            print("decode error")
+        }
+        return nil
+    }
+    
 }
