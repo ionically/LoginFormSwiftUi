49d36cb4d14c37b2aa9a2075ac89ac2304f80860
mspoonam <mspoonam.learning@gmail.com>
2022-10-26 21:56:24 -0400

    Adding `login-api-response` json file Renaming `ContentView` to `LoginView`

--- LoginScreen/UI/Login/LoginView.swift
+++ LoginScreen/UI/Login/LoginView.swift
@@ -7,13 +7,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var username = ""
-    @State private var password = ""
+struct LoginView: View {
+    @ObservedObject private(set) var viewModel: LoginViewModel
+    
     @State private var wrongUsername: Float = 0
     @State private var wrongPassword: Float  = 0
     @State private var showingLoginScreen = false
-    
     
     var body: some View {
         NavigationView {
@@ -81,6 +80,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
---
+++ LoginScreen/UI/Login/LoginViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewModel.swift
+//  LoginScreen
+//
+//  Created by Poonam Pandey on 26/10/22.
+//
+
+import Foundation
+
+public class LoginViewModel: ObservableObject {
+    
+}
+
+struct LoginFormState {
+    var username: String = ""
+    var password: String = ""
+}
